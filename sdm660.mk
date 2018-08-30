16232595c76f318ffeac03c1a709b604bb507ad1
Brinly T <brinly@brinly.me>
2018-08-30 13:59:43 +1000

    sdm660-common: Add health package

--- sdm660.mk
+++ sdm660.mk
@@ -107,6 +107,11 @@ PRODUCT_PACKAGES += \
     libvulkan \
     memtrack.sdm660
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-service
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
