bd5a35d94ce6a3ae2b5b32274da7802ca7fa6aad
Brinly Taylor <brinly@brinly.me>
2018-10-27 23:05:17 +0000

    sdm660-common: let's make this real treble
    This needs BQ 1.6 based firmware or later to function correctly.
    
    Change-Id: If2c45a62ae5d9c5bc794c54b7f32f2e5e9eee577

--- sdm660.mk
+++ sdm660.mk
@@ -112,7 +112,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
-    android.hardware.camera.common@1.0-helper \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libxml2 \
