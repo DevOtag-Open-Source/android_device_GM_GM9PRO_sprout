81fd682675415e8f64ae531eab9edb8562bdc02c
Eloi Carracedo <eloimuns@gmail.com>
2019-09-30 23:58:29 +0200

    sdm660-common: Add missing radio packages
    Change-Id: I14d9101a5d238910cfef2832b97361931d50e4af

--- sdm660.mk
+++ sdm660.mk
@@ -309,6 +309,9 @@ PRODUCT_PACKAGES += \
 
 # Radio
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.2 \
+    android.hardware.radio.config@1.0 \
+    android.hardware.secure_element@1.0 \
     librmnetctl
 
 # RCS
