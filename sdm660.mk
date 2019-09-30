942512840c83daf3420263b6da8db61624732865
Eloi Carracedo <eloimuns@gmail.com>
2019-09-30 18:51:38 +0200

    sdm660-common: Remove libicuuc.vendor
     Also don't build vndk_package
    
    Change-Id: I35103d7c547fd9b72978ee360b1b7d73e09fd8c4

--- sdm660.mk
+++ sdm660.mk
@@ -385,12 +385,9 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 # Update this list with what each blob is actually for
-# libicuuc: vendor.qti.hardware.qteeconnector@1.0-impl
 # libstdc++: hexagon DSP blobs
 PRODUCT_PACKAGES += \
-    libicuuc.vendor \
     libstdc++.vendor \
-    vndk_package \
     vndk-sp
 
 # Wi-Fi
