a081e1a729406cb82de85ed1da6d1a5ed806de57
Eloi Carracedo <eloimuns@gmail.com>
2019-10-06 15:10:34 +0200

    sdm660-common: Update camera props
    Change-Id: I5cabfa2442403cbc860c6148091c1fc8014381e9

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -60,6 +60,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.camera.aux.packagelist="org.codeaurora.snapcam,com.bq.camerabq,com.bq.camerabq.debug,org.lineageos.snap" \
     vidc.enc.dcvs.extra-buff-count=2 \
+    persist.vendor.camera.af.sync=2 \
+    persist.vendor.camera.awb.sync=2 \
     persist.vendor.camera.customer.config=zangya_camera.xml \
     persist.vendor.camera.exif.rotation=off \
     persist.vendor.camera.expose.aux=1 \
@@ -67,6 +69,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.imglib.usefdlite=1 \
     persist.vendor.camera.lib2d.rotation=on \
     persist.vendor.camera.isp.clock.optmz=0 \
+    persist.vendor.camera.preview.ubwc=0 \
     vendor.video.disable.ubwc=1
 
 # CABL
